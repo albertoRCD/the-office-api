be13545cc4eee0cb6f2b18bb27482a3918cf1cd5
Alberto Varela <lolo.avp@gmail.com>
2022-05-02 10:50:20 +0200

    agrego de imagenes

--- src/Item.js
+++ src/Item.js
@@ -5,9 +5,14 @@ export const Item = (props) => {
     <div>
 
     <h1>{props.id}. {props.name}</h1>
+    <div>
+    <img src={`./images/${props.id}.jpg`} alt={props.name} />
+    </div>
 
     <h2>Real Name: {props.realname}</h2>
+
     <h2>Seasons: {props.seasons}</h2>
+    
 
     
     </div>
